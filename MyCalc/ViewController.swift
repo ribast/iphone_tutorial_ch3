490448033728608dde8905979af5988a27c3fa8b
Ryo Tajima <ryo.tajima@keio.jp>
2018-12-01 21:33:27 +0900

    数字ボタンにaction実装

--- MyCalc/ViewController.swift
+++ MyCalc/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,85 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tap1Button(_ sender: Any) {
+        let value = priceFied.text! + "1"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
 
+    @IBAction func tap2Button(_ sender: Any) {
+        let value = priceFied.text! + "2"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+
+    @IBAction func tap3Button(_ sender: Any) {
+        let value = priceFied.text! + "3"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+
+    @IBAction func tap4Button(_ sender: Any) {
+        let value = priceFied.text! + "4"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+
+    @IBAction func tap5Button(_ sender: Any) {
+        let value = priceFied.text! + "5"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        let value = priceFied.text! + "6"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        let value = priceFied.text! + "7"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        let value = priceFied.text! + "8"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        let value = priceFied.text! + "9"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap0Button(_ sender: Any) {
+        let value = priceFied.text! + "0"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tap00Button(_ sender: Any) {
+        let value = priceFied.text! + "00"
+        if let price = Int(value) {
+            priceFied.text = "\(price)"
+        }
+    }
+    
+    @IBAction func tapClearButton(_ sender: Any) {
+        priceFied.text = "0"
+    }
 }
 
